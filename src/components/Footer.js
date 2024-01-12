de903a89f5e3ae17ce9c232ca1ea42c398128f89
sonalidalai11 <156323847+sonalidalai11@users.noreply.github.com>
2024-01-12 08:35:12 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
     <footer className="bg-primary py-12">
       <div className="container mx-auto">
         <p className="text-white text-center">
-          Copyright &copy; Neelam Padhy 🤍 2023. All rights reserved.
+          Copyright &copy; SONALI DALAI 🤍 2024. All rights reserved.
         </p>
       </div>
     </footer>
